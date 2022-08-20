fc02e372a07faac767510de445b19e739d9ef022
tok3rat0r <owenmjones1@gmail.com>
2022-08-20 16:56:54 +0700

    Convert spaces to tabs and normalize files

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Hello from Flask!"
+	return "Hello from Flask!"
--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import SimpleMap from "./simpleMap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>#BTW</h1>
-      </header>
-      <div>
-        <SimpleMap className="Map"/>
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>#BTW</h1>
+			</header>
+			<div>
+				<SimpleMap className="Map"/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
