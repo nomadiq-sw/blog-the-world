9a7bde29d14dd7713653f11ba3daef0d030f9adb
tok3rat0r <owenmjones1@gmail.com>
2022-08-20 17:02:12 +0700

    Add first unit test to React app

--- react-frontend/src/App.test.js
+++ react-frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders site header', () => {
 	render(<App />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+	const titleElement = screen.getByText(/#BTW/i);
+	expect(titleElement).toBeInTheDocument();
 });
